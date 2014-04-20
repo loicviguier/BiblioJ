f29ff89d36349635707ec9741204e47701ddc163
loicviguier <loicviguier@gmail.com>
2014-04-20 17:52:54 +0200

    Restriction user view, controller, Livre et Auteur

--- grails-app/controllers/biblioj/AuteurController.groovy
+++ grails-app/controllers/biblioj/AuteurController.groovy
@@ -15,20 +15,20 @@ class AuteurController {
         [auteurInstanceList: Auteur.list(params), auteurInstanceTotal: Auteur.count()]
     }
 
-    def create() {
-        [auteurInstance: new Auteur(params)]
-    }
+//    def create() {
+//        [auteurInstance: new Auteur(params)]
+//    }
 
-    def save() {
-        def auteurInstance = new Auteur(params)
-        if (!auteurInstance.save(flush: true)) {
-            render(view: "create", model: [auteurInstance: auteurInstance])
-            return
-        }
-
-        flash.message = message(code: 'default.created.message', args: [message(code: 'auteur.label', default: 'Auteur'), auteurInstance.id])
-        redirect(action: "show", id: auteurInstance.id)
-    }
+//    def save() {
+//        def auteurInstance = new Auteur(params)
+//        if (!auteurInstance.save(flush: true)) {
+//            render(view: "create", model: [auteurInstance: auteurInstance])
+//            return
+//        }
+//
+//        flash.message = message(code: 'default.created.message', args: [message(code: 'auteur.label', default: 'Auteur'), auteurInstance.id])
+//        redirect(action: "show", id: auteurInstance.id)
+//    }
 
     def show(Long id) {
         def auteurInstance = Auteur.get(id)
@@ -41,62 +41,62 @@ class AuteurController {
         [auteurInstance: auteurInstance]
     }
 
-    def edit(Long id) {
-        def auteurInstance = Auteur.get(id)
-        if (!auteurInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
-            redirect(action: "list")
-            return
-        }
+//    def edit(Long id) {
+//        def auteurInstance = Auteur.get(id)
+//        if (!auteurInstance) {
+//            flash.message = message(code: 'default.not.found.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
+//            redirect(action: "list")
+//            return
+//        }
+//
+//        [auteurInstance: auteurInstance]
+//    }
 
-        [auteurInstance: auteurInstance]
-    }
+//    def update(Long id, Long version) {
+//        def auteurInstance = Auteur.get(id)
+//        if (!auteurInstance) {
+//            flash.message = message(code: 'default.not.found.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
+//            redirect(action: "list")
+//            return
+//        }
+//
+//        if (version != null) {
+//            if (auteurInstance.version > version) {
+//                auteurInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+//                          [message(code: 'auteur.label', default: 'Auteur')] as Object[],
+//                          "Another user has updated this Auteur while you were editing")
+//                render(view: "edit", model: [auteurInstance: auteurInstance])
+//                return
+//            }
+//        }
+//
+//        auteurInstance.properties = params
+//
+//        if (!auteurInstance.save(flush: true)) {
+//            render(view: "edit", model: [auteurInstance: auteurInstance])
+//            return
+//        }
+//
+//        flash.message = message(code: 'default.updated.message', args: [message(code: 'auteur.label', default: 'Auteur'), auteurInstance.id])
+//        redirect(action: "show", id: auteurInstance.id)
+//    }
 
-    def update(Long id, Long version) {
-        def auteurInstance = Auteur.get(id)
-        if (!auteurInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
-            redirect(action: "list")
-            return
-        }
-
-        if (version != null) {
-            if (auteurInstance.version > version) {
-                auteurInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'auteur.label', default: 'Auteur')] as Object[],
-                          "Another user has updated this Auteur while you were editing")
-                render(view: "edit", model: [auteurInstance: auteurInstance])
-                return
-            }
-        }
-
-        auteurInstance.properties = params
-
-        if (!auteurInstance.save(flush: true)) {
-            render(view: "edit", model: [auteurInstance: auteurInstance])
-            return
-        }
-
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'auteur.label', default: 'Auteur'), auteurInstance.id])
-        redirect(action: "show", id: auteurInstance.id)
-    }
-
-    def delete(Long id) {
-        def auteurInstance = Auteur.get(id)
-        if (!auteurInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
-            redirect(action: "list")
-            return
-        }
-
-        try {
-            auteurInstance.delete(flush: true)
-            flash.message = message(code: 'default.deleted.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
-            redirect(action: "show", id: id)
-        }
-    }
+//    def delete(Long id) {
+//        def auteurInstance = Auteur.get(id)
+//        if (!auteurInstance) {
+//            flash.message = message(code: 'default.not.found.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
+//            redirect(action: "list")
+//            return
+//        }
+//
+//        try {
+//            auteurInstance.delete(flush: true)
+//            flash.message = message(code: 'default.deleted.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
+//            redirect(action: "list")
+//        }
+//        catch (DataIntegrityViolationException e) {
+//            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'auteur.label', default: 'Auteur'), id])
+//            redirect(action: "show", id: id)
+//        }
+//    }
 }
